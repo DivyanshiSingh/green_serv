0ba0f1b18ed6b6b1a16a551d93367b54720e3348
Divyanshi Singh <divyanshipilot@gmail.com>
2020-10-27 11:23:15 +0530

    new files are added

---
+++ src/Components/Navbar/MenuItem.js
@@ -0,0 +1,32 @@
+export const MenuItems=[
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Services',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Portfolio',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Blog',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact',
+        url: '#',
+        cName: 'nav-links'
+    },
+]
