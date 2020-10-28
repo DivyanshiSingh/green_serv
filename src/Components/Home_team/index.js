78051eb74277bf26d7ad72cf52a1d794df43e6ee
manas2297 <yadavmanas22@gmail.com>
2020-10-29 00:56:59 +0530

    Stepper added and minor design fixes

--- src/Components/Home_team/index.js
+++ src/Components/Home_team/index.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import './Home_team.css';
+import React from "react";
+import Stepper from "../Stepper";
+import "./Home_team.css";
 
-function Home_team() {
-    return (
-    <div className="team">
+function HomeTeam() {
+  return (
+    <div className="container">
+      <div className="team">
         <div className="heading">
-            <h1>A team United by Shared Values</h1>
-        </div>
+          <hr />
+          <h1> A Team United by Shared Values </h1>{" "}
+        </div>{" "}
         <div className="outer_team">
-            <div className="number_outer">
-                <p className="number">4</p>
-                <h1 className="number_tag">Professional Designers in our team</h1>
+          <div className="number_outer">
+            <p className="number"> 4 </p>{" "}
+            <h1 className="number_tag"> Professional Designers in our team </h1>{" "}
+          </div>{" "}
+          <div className="image_outer">
+            <div className="image_line1">
+              <div className="team_img1"> </div>{" "}
+              <div className="team_img2"> </div>{" "}
             </div>
-            <div className="image_outer">
-                <div className="image_line1">
-                    <div className="team_img1">
-
-                    </div>
-                    <div className="team_img2">
-
-                    </div>
-
-                </div>
-
-                <div className="image_line2">
-                    <div className="team_img3">
-
-                    </div>
-                    <div className="team_img4">
-
-                    </div>
-                </div>
-            </div>
+            <div className="image_line2">
+              <div className="team_img3"> </div>{" "}
+              <div className="team_img4"> </div>{" "}
+            </div>{" "}
+          </div>{" "}
+        </div>{" "}
+      </div>
+      <div className="process">
+        <div className="heading">
+          <hr />
+          <h1>Our process to make you dream true</h1>
         </div>
-        
+        <Stepper/>
+      </div>
     </div>
-    )
+  );
 }
 
-export default Home_team;
+export default HomeTeam;
