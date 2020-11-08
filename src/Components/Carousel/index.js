4cae8327703f7cf3597751ac830e8d0f6a9ec2fe
manas2297 <yadavmanas22@gmail.com>
2020-11-08 06:46:39 +0530

    testimonial added

--- src/Components/Carousel/index.js
+++ src/Components/Carousel/index.js
@@ -1,26 +1,7 @@
-import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+
 import React from 'react';
 import Slider from 'react-slick';
-import Service1 from '../../images/service1.jpg';
-import Service2 from '../../images/service2.jpg';
-import Service3 from '../../images/service3.jpg';
-import Service4 from '../../images/service4.jpg';
 import './styles.css';
-const NextArr = (props) => {
-  return (
-    <div className="arr_next"  {...props}>
-      <ArrowForwardIosIcon />
-    </div>
-  )
-}
-const PrevArr = (props) => {
-  return (
-    <div {...props}>
-      <ArrowBackIosIcon/>
-    </div>
-  )
-}
 const Carousel = (props) => {
   const settings = {
     dots: true,
@@ -28,33 +9,12 @@ const Carousel = (props) => {
     speed: 500,
     slidesToScroll: 1,
     slidesToShow: 2,
-    nextArrow: <NextArr/>,
-    prevArrow: <PrevArr/>
   };
   return (
     <div className="car_container">
-      <Slider {...settings}>
-        <div>
-          <div className="carousel_img">
-            <img src={Service1} alt= "service1" />
-          </div>
-          </div>
-          <div>
-          <div className="carousel_img">
-            <img src={Service2} alt= "service1" />
-          </div>
-          </div>
-          <div>
-          <div className="carousel_img">
-            <img src={Service3} alt= "service1" />
-          </div>
-          </div>
-          <div>
-          <div className="carousel_img">
-            <img src={Service4} alt= "service1" />
-          </div>
-          </div>
-        </Slider>
+      <Slider {...props.settings}>
+          {props.children}
+      </Slider>
     </div>
   );
 };
---
+++ src/Components/Projects/PrevArr.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+const PrevArr = (props) => {
+  return (
+    <div {...props}>
+      <ArrowBackIosIcon/>
+    </div>
+  )
+}
+export default PrevArr;
