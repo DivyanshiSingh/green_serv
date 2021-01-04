2f381ad7336c5846ec00589388bdbf6e52222308
manas2297 <yadavmanas22@gmail.com>
2021-01-04 16:19:39 +0530

    portfolio responsive

--- src/Components/Handles/index.js
+++ src/Components/Handles/index.js
@@ -7,15 +7,15 @@ function Handles() {
   return (
     <div className="sticky_handles">
       <div className="icon2">
-      <Insta style={{"fontSize": 35, "color": "#2b6b68"}}/>
+      <Insta style={{"fontSize": 29, "color": "#2b6b68"}}/>
         <a href="https://www.instagram.com/p/CHUkVjEjk5e/?igshid=xucg687ifvhq">GreenServ</a>
       </div>
       <div className="icon2">
-      <Fb  style={{"fontSize": 35, "color": "#2b6b68"}}/>
+      <Fb  style={{"fontSize": 29, "color": "#2b6b68"}}/>
         <a href="https://www.facebook.com/GreenservInteriors">GreenservInteriors</a>
       </div>
       <div className="icon2">
-        <Whatsapp style={{"fontSize": 35, "color": "#2b6b68"}}/>
+        <Whatsapp style={{"fontSize": 29, "color": "#2b6b68"}}/>
         <a>8373989676</a>
       </div>
     </div>
